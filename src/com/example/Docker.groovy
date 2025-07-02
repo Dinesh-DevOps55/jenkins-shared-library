f355f53e9d3154bd322b2387c4d149e6c773df37
GIT Tuttorial <dineshs.siva@gmail.com>
2025-07-02 20:19:30 +0530

    docker groovy chnages

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,20 @@
+#!/usr/bin/env groovy
+package com.example
+
+class Docker implements Serializable {
+
+    def script
+
+    Docker(script) {
+        this.script = script //This script will allow us to execute pipeline commands.
+    }
+
+    def buildDockerImage(String imageName) {
+        script.echo "Building the Docker image..."
+        script.withCredentials([ script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "docker build -t $imageName ."
+            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
+            script.sh "docker push $imageName"
+        }
+    }
+}
